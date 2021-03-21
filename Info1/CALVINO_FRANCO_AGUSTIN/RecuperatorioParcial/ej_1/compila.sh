ccba44a0e1053f1e2ec5c4164e7fd0f37654cccb
Franco Calvino <frcalvino@est.frba.utn.edu.ar>
2021-03-21 15:59:12 -0300

    gfjkjgk

--- Info1/CALVINO_FRANCO_AGUSTIN/RecuperatorioParcial/ej_1/compila.sh
+++
@@ -1,4 +0,0 @@
-gcc -c main1.c -o main1.o -Wall
-gcc -c funciones1.c -o funciones1.o -Wall
-gcc main1.o funciones1.o -o ejercicio1 -Wall
-./ejercicio1
--- Info1/CALVINO_FRANCO_AGUSTIN/RecuperatorioParcial/ej_3/compila.sh
+++
@@ -1,4 +0,0 @@
-gcc -c main3.c -o main3.o -Wall
-gcc -c funciones3.c -o funciones3.o -Wall
-gcc main3.o funciones3.o -o ejercicio3 -lm -Wall
-./ejercicio3
